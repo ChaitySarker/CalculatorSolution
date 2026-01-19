4f1680272503c05282d8a9aabd222e9be8a511b5
Chaity <Chaity@DEV-1015>
2026-01-19 12:11:19 +0600

    Add project files.

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using CalculatorApp;
+using Xunit;
+
+public class CalculatorTests
+{
+    Calculator calc = new Calculator();
+
+    [Fact]
+    public void Add_Works() => Assert.Equal(5, calc.Add(2, 3));
+
+    [Fact]
+    public void Sub_Works() => Assert.Equal(1, calc.Sub(3, 2));
+
+    [Fact]
+    public void Mul_Works() => Assert.Equal(6, calc.Mul(2, 3));
+
+    [Fact]
+    public void Div_Works() => Assert.Equal(2, calc.Div(4, 2));
+
+    [Fact]
+    public void Div_ByZero_Throws() =>
+        Assert.Throws<DivideByZeroException>(() => calc.Div(5, 0));
+}
---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+        public double Sub(double a, double b) => a - b;
+        public double Mul(double a, double b) => a * b;
+        public double Div(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
